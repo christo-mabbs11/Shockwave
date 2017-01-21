44303f2b0c2f5139e04474e0522c09699e051d3f
christo-mabbs11 <christo.mabbs@gmail.com>
2017-01-21 23:10:47 +1030

    more progress

--- Assets/Scripts/GrenadeManager.cs
+++ Assets/Scripts/GrenadeManager.cs
@@ -6,42 +6,58 @@ public class GrenadeManager : MonoBehaviour {
 	public float GrenadePush = 45.0f;
 	public float GrenadeRadius = 10.0f;
 
+	GameObject BombRef;
+
+	void Awake () {
+
+		BombRef = GameObject.FindGameObjectWithTag ("Bomb");
+
+		// Grenade ignores collisions with the bomb
+		Physics2D.IgnoreCollision(this.gameObject.GetComponent<CircleCollider2D>(), BombRef.GetComponent<BoxCollider2D>());
+	}
 
 	// If the grenade collides with something
 	void OnCollisionEnter2D(Collision2D coll) {
 
-		// Ignores players
-		if ( coll.gameObject.tag != "Player" ) {
+		// Ignores players and the bomb
+		if ( coll.gameObject.tag != "Player" && coll.gameObject.tag != "Bomb" ) {
 
 			// Loop through all the player objects whithin a certain radius
 			GameObject[] Players = GameObject.FindGameObjectsWithTag("Player");
 			foreach (GameObject player in Players) {
-
-				float PlayerDistance = Mathf.Abs(Vector3.Distance (transform.position, player.transform.position));
-				Debug.Log ( PlayerDistance );
-
-				if ( PlayerDistance <= GrenadeRadius ) {
-
-					// Find the angle of the blast
-					Vector2 BlastAngle = new Vector2( player.transform.position.x - this.transform.position.x, player.transform.position.y - this.transform.position.y );
-
-					Vector3 BlastAngleNormalized = Vector3.Normalize ( new Vector3( BlastAngle.x, BlastAngle.y, 0.0f) );
-					Vector2 BlastAngleNormalized_2D = new Vector2( BlastAngleNormalized.x, BlastAngleNormalized.y );
-
-					// Set the players current velocity to 0
-					player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-
-					// Apply blast, scaled for distance
-					player.GetComponent<Rigidbody2D>().AddForce( GrenadePush * (1.0f - PlayerDistance / GrenadeRadius) * BlastAngleNormalized_2D, ForceMode2D.Impulse );
-
-					// Disable the movement of the player being hit
-					player.GetComponent<CharacterManager> ().GrenadeDisableMovement ();
-
-				}
+				BlowObject ( player, GrenadePush );
 			}
+
+			// Blow up the bomb
+			BlowObject ( BombRef, GrenadePush * 0.14f );
 
 			// Destroy this grenade
 			Destroy (this.gameObject);
+		}
+	}
+
+	void BlowObject ( GameObject Arg_GameObject, float Arg_GrenadePush ) {
+		
+		float GameObjectDistance = Mathf.Abs(Vector3.Distance (transform.position, Arg_GameObject.transform.position));
+
+		if ( GameObjectDistance <= GrenadeRadius ) {
+
+			// Find the angle of the blast
+			Vector2 BlastAngle = new Vector2( Arg_GameObject.transform.position.x - this.transform.position.x, Arg_GameObject.transform.position.y - this.transform.position.y );
+
+			Vector3 BlastAngleNormalized = Vector3.Normalize ( new Vector3( BlastAngle.x, BlastAngle.y, 0.0f) );
+			Vector2 BlastAngleNormalized_2D = new Vector2( BlastAngleNormalized.x, BlastAngleNormalized.y );
+
+			// Set the players current velocity to 0
+			Arg_GameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+			// Apply blast, scaled for distance
+			Arg_GameObject.GetComponent<Rigidbody2D>().AddForce( Arg_GrenadePush * (1.0f - GameObjectDistance / GrenadeRadius) * BlastAngleNormalized_2D, ForceMode2D.Impulse );
+
+			// Disable the movement if a player is being hit
+			if ( Arg_GameObject.tag == "Player" ) {
+				Arg_GameObject.GetComponent<CharacterManager> ().GrenadeDisableMovement ();
+			}
 		}
 	}
 }
